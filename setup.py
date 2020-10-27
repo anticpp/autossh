4eb7b7c7cdd2be38e1e6b6d5b37c5c85387b00ab
supergui <supergui@live.cn>
2020-10-27 10:31:58 +0800

    Upgrade version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name="autossh",
-        version="1.4.1",
+        version="1.4.2",
         description="Auto-SSH toolkits",
         url="https://www.github.com/anticpp/autossh",
         author="supergui",
